691cb9588205fbdd7c558b7417b4b55a5e4759a1
Grasieli Wiedthauger <grasieli.wie@hotmail.com>
2024-08-15 10:52:51 -0300

    alterações finais

--- exercicio3-parte2/exercicio3-parte2.go
+++ exercicio3-parte2/exercicio3-parte2.go
@@ -2,28 +2,16 @@ package main
 
 import "fmt"
 
-//Repita a lógica de comparação do exercício anterior só que dessa vez para uma lista de números.
-//Nesse exercício não iremos retornar uma string, em vez disso, caso o número for maior que zero some 10
-//e printe o resultado; caso for igual, some 2 e printe o resultado;
-//caso for menor, some 23 e printe o resultado
-
 func main() {
 
-	//declarar lista
 	lista := []int{1, 2, 3, 4, 5}
 
-	// criar variavel de retorno e adicionar os parametros
 	resultadoDisplay := TesteValor(lista)
 
 	fmt.Println(resultadoDisplay)
 }
 
 func TesteValor(n []int) []int {
-
-	//le os valores do slice
-	//compara os valores lidos
-	//calcula o resultado
-	//retorna o resultado
 
 	for i, num := range n {
 		if num > 0 {
